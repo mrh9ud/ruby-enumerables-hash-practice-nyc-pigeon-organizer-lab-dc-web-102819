77c7d8fee1278b802a0dfac8ef035bdaad98344e
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-24 17:13:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,20 +7,20 @@ def nyc_pigeon_organizer(data)
       #puts inner_keys
       #puts nested_arrays
       nested_arrays.each do |bird_names|
-        puts bird_names
+        #puts bird_names
         if !final_hash.has_key?(bird_names)
           final_hash[bird_names] = {}
         end
-        if !final_hash[bird_names].has_key?(main_keys)
-          final_hash[bird_names][main_keys] = []
-        end
-        if !final_hash[bird_names][main_keys].include?(inner_keys)
-          final_hash[bird_names][main_keys].push(inner_keys).to_s
-        end
+        #if !final_hash[bird_names].has_key?(main_keys)
+        #  final_hash[bird_names][main_keys] = []
+        #end
+        #if !final_hash[bird_names][main_keys].include?(inner_keys)
+         # final_hash[bird_names][main_keys].push(inner_keys).to_s
+        #end
       end  
     end
   end
-  #puts final_hash
+  puts final_hash
 end
 nyc_pigeon_organizer({
         :color => {
