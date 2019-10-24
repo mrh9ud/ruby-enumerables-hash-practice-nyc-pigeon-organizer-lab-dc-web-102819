e3e47f88e6dd99bc1e2d28c44cf350458b1dd4b9
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-24 16:37:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |property, hash|
     puts property
-    puts hash
+    #puts hash
     hash.each do |attribute, array|
       array.each do |name|
         if !new_hash.has_key?(name)
