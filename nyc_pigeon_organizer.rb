9d1e21ce78ce03334b483fd3a7ceb98350bf5089
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-24 16:24:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -29,40 +29,21 @@ end
 #  end
 #  new_hash
 #end
-nyc_pigeon_organizer({
-  "Theo" => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Subway"]
-  },
-  "Peter Jr." => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Library"]
-  },
-  "Lucky" => {
-    :color => ["purple"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
-  },
-  "Ms .K" => {
-    :color => ["grey", "white"],
-    :gender => ["female"],
-    :lives => ["Central Park"]
-  },
-  "Queenie" => {
-    :color => ["white", "brown"],
-    :gender => ["female"],
-    :lives => ["Subway"]
-  },
-  "Andrew" => {
-    :color => ["white"],
-    :gender => ["male"],
-    :lives => ["City Hall"]
-  },
-  "Alex" => {
-    :color => ["white", "brown"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
-  }
-})
+nyc_pigeon_organizer(      pigeon_data = {
+        :color => {
+          :purple => ["Theo", "Peter Jr.", "Lucky"],
+          :grey => ["Theo", "Peter Jr.", "Ms. K"],
+          :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+          :brown => ["Queenie", "Alex"]
+        },
+        :gender => {
+          :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+          :female => ["Queenie", "Ms. K"]
+        },
+        :lives => {
+          "Subway" => ["Theo", "Queenie"],
+          "Central Park" => ["Alex", "Ms. K", "Lucky"],
+          "Library" => ["Peter Jr."],
+          "City Hall" => ["Andrew"]
+        }
+      })
